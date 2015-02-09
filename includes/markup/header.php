34f8eaef2c311b344570e946d07826fecd1ebf7b
erik--vink <erik.vink95@gmail.com>
2015-02-09 17:50:18 +0100

    Completed V1 of header/footer
    Completed V1 of header/footer

--- includes/markup/header.php
+++ includes/markup/header.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="/SOSRommelmarkt/includes/css/menu.css" type="text/css" media="screen">
     <link rel="stylesheet" href="/SOSRommelmarkt/includes/fonts/font-awesome/css/font-awesome.min.css" >
     <link href = "/SOSRommelmarkt/includes/css/bootstrap.min.css" rel = "stylesheet">
+    <link href = "/SOSRommelmarkt/includes/css/shoppingcart.css" rel = "stylesheet">
+    <link href = "/SOSRommelmarkt/includes/css/footer.css" rel = "stylesheet">
     
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -17,6 +19,7 @@
     <![endif]-->    
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="/SOSRommelmarkt/includes/js/menu.js" type="text/javascript"></script>
+    <script src="/SOSRommelmarkt/includes/js/shoppingcart.js" type="text/javascript"></script>
     <script src="/SOSrommelmarkt/includes/js/bootstrap.js"></script>
     
     
@@ -24,6 +27,78 @@
 <body>
 
     <div class="container">
+    
+        
+    <div class="col-sm-3 col-md-6"></div>
+    <div class="col-sm-3 col-md-3">
+            <form class="navbar-form" role="search">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Zoeken" name="srch-term" id="srch-term">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+            </form>
+    </div>
+            
+    <div class="col-sm-3 col-md-3">
+        
+            <div id="cart_details_3">
+              <div class="cart_btn">
+                <span class="icon"><i class="fa fa-shopping-cart"></i></span>
+                <div class="items"><span>2 producten</span></div>
+                <div class="price"><span>&euro; 27,95</span></div>
+              </div>
+              <div class="details">
+                <div class="product_row">
+                  <div class="image">
+                     <img id="cart_image" src="http://i.imgur.com/gJsXvHB.jpg"/>
+                  </div>
+                  <div class="info">
+                    <span class="title">Pink Halter Small</span>
+                    <span class="price">$27.95</span>
+                    <div class="qty"><input id="qty" placeholder="1" disabled></input></div>
+                    <div class="remove"><i class="fa fa-times-circle"></i></div>
+                  </div>
+                </div>
+                <div class="product_row">
+                  <div class="image">
+                    <img id="cart_image" src="http://i.imgur.com/gJsXvHB.jpg"/>
+                  </div>
+                  <div class="info">
+                    <span class="title">Pink Halter Small</span>
+                    <span class="price">$27.95</span>
+                    <div class="qty"><input id="qty" placeholder="1" disabled></input></div>
+                    <div class="remove"><i class="fa fa-times-circle"></i></div>
+                  </div>
+                </div>
+                <div class="bottom">
+                  <div class="left">
+                      <p>Totaal: &euro; 27,95</p>
+                    
+                  </div>
+                  <div class="right">
+                    <a class="checkout" href="#">Bestellen <i class="fa fa-chevron-right"></i></a>
+                  </div>
+               </div>
+            </div>
+          </div>
+
+        
+        
+    </div>
+    </div>    
+    
+    
+    <div class="container">
+    
+        
+        
+        
+    <img id="logo" src="/SOSRommelmarkt/img/logo_sos.png" />    
+    
+        
+    <div>  
     <a id="touch-menu" class="mobile-menu" href="#"><i class="fa fa-bars"></i></a>
     
     <nav>
@@ -53,6 +128,8 @@
             <li><a  href="#"><i class="fa fa-envelope"></i> CONTACT</a></li>
       </ul>
     </nav>
+    </div>
+        
     </div>
     
     
