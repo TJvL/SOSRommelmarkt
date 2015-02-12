e05d823fd2aad38427a7dd56d804e61f67ca8570
thomas <tjleeuwe1@student.avans.nl>
2015-02-13 00:28:02 +0100

    Contact pagina

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!--For best practice on includes, a config php file is required which defines the root, so absolute paths can be created from $root/includes/markup for example..-->
+<?php include("includes/markup/header.php"); ?> 
+
+<div class="container">
+            <div class= "col-md-12">
+                
+				<div class="title">
+					<h2>Contact</h2>
+				</div>
+				<ul class="style1">
+						Vughterstraat 264-A</br>
+						5211 GR 's Hertogenbosch </br>
+						tel: 073 613 3774 </br>
+						email: info@sosrommelmarkt.nl             
+				</ul>
+                
+				<div class="title">
+					<h2>Route naar winkel:</h2>
+				</div>
+				
+				<p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2472.794347019211!2d5.301413096300931!3d51.7002083979153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1snl!2snl!4v1409405577484" width="800" height="600" frameborder="0" style="border:0"></iframe><br />
+				<small><a style="color: #0000ff; text-align: left;" href="http://maps.google.nl/maps?hl=nl&amp;ie=UTF8&amp;hq=&amp;hnear='s-Hertogenbosch,+Noord-Brabant&amp;ll=51.684337,5.295502&amp;spn=0.010497,0.01929&amp;t=m&amp;z=16&amp;source=embed">Grotere kaart weergeven</a></small></p>
+				
+            </div>
+</div>
+
+<?php
+include("includes/markup/footer.php"); 
+?> 
