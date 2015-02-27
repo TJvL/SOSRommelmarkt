777bd856853100b1e345956e911f94180f7de059
BramBrouwer <b.brouwer@student.avans.nl>
2015-02-27 10:31:27 +0100

    Contactformulier en actiepagina
    daadwerkelijk opslaan moet nog in de actiepagina gezet worden

---
+++ contact_action.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bram
+ * Date: 2/27/2015
+ * Time: 10:28 AM
+ */
+//hier komt de funtionaliteit van de info opslaan in de database met behulp van database.php
