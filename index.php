a53b560c5692a14f780a165e830956ed7e47214f
thomas <tjleeuwe1@student.avans.nl>
2015-03-02 21:50:01 +0100

    index reverted

--- index.php
+++ index.php
@@ -1,24 +1,47 @@
 <!--For best practice on includes, a config php file is required which defines the root, so absolute paths can be created from $root/includes/markup for example..-->
-<?php include("includes/markup/header.php"); ?> 
+<?php include("includes/markup/header.php"); ?>
 
-   <div class= "container">
-      
-    </div>
-		
-		<div class="container">
-            
-			<div class="white">
-            <!--Placeholders for relevant content-->
+<div class= "container">
+
+</div>
+
+<div class="container">
+
+    <div class="grey">
+
+        <div class="row">
+
+            <div class= "col-md-3">
+
+                <img class="img-responsive" src="/SOSRommelmarkt/img/headerdiv.png">
+
 
             </div>
-				
-		</div>
 
-    
-    
-    
-    
+            <div class="col-md-9">
+
+                <p>Alles wat verkoopbaar is krijgt bij klanten een nieuw leven.</p>
+                <p>Het restafval wordt gescheiden aangeleverd bij verwerkingsbedrijven.</p>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="white">
+        <!--Placeholders for relevant content-->
+        <div class="row">
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
 <?php
-include("includes/markup/footer.php"); 
-?>     
+include("includes/markup/footer.php");
+?>
    
