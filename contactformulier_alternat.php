70fcc1dc9fa33c4ccd8d37f44d62542930f1a10b
Simon van der Meer <simon_vd_meer@live.nl>
2015-03-06 10:13:23 +0100

    Merge branch 'development' of https://github.com/TJvL/SOSRommelmarkt into development
    Conflicts:
    	.gitignore

--- contactformulier_alternat.php
+++ contactformulier_alternat.php
@@ -12,146 +12,152 @@
 </head>
 
 <body>
-<div class="content">
+<div class="container">
+<div class="white">
+    <div class="content">
 
-    <div class="idealsteps-container">
+        <div class="idealsteps-container">
 
-        <nav class="idealsteps-nav"></nav>
+            <nav class="idealsteps-nav"></nav>
 
-        <form method="POST" action="contact_action.php"  class="idealforms" name="aanvraag">
+            <form method="POST" action="contact_action.php"  class="idealforms" name="aanvraag">
 
-            <div class="idealsteps-wrap">
+                <div class="idealsteps-wrap">
 
-                <!-- Step 1 -->
+                    <!-- Step 1 -->
 
-                <section class="idealsteps-step">
-
-
-                    <div class="field">
-                        <label class="main">voornaam</label>
-                        <input name="name" type="text" placeholder="Uw voornaam">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">Achternaam</label>
-                        <input name="name" type="text" placeholder="Uw achternaam">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">Bedrijfsnaam</label>
-                        <input name="bedrijfsnaam" type="text" placeholder="De naam van uw bedrijf">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">KVK nummer</label>
-                        <input name="kvknr" type="text" placeholder="Uw KVK-nummer">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field buttons">
-                        <label class="main">&nbsp;</label>
-                        <button type="button" class="next">Volgende &raquo;</button>
-                    </div>
+                    <section class="idealsteps-step">
 
 
-                </section>
+                        <div class="field">
+                            <label class="main">voornaam</label>
+                            <input name="name" type="text" placeholder="Uw voornaam">
+                            <span class="error"></span>
+                        </div>
 
-                <!-- Step 2 -->
+                        <div class="field">
+                            <label class="main">Achternaam</label>
+                            <input name="name" type="text" placeholder="Uw achternaam">
+                            <span class="error"></span>
+                        </div>
 
-                <section class="idealsteps-step">
+                        <div class="field">
+                            <label class="main">Bedrijfsnaam</label>
+                            <input name="bedrijfsnaam" type="text" placeholder="De naam van uw bedrijf">
+                            <span class="error"></span>
+                        </div>
 
-                    <div class="field">
-                        <label class="main">Straatnaam</label>
-                        <input name="straatnaam" type="text" placeholder="Uw straatnaam met huisnummer">
-                        <span class="error"></span>
-                    </div>
+                        <div class="field">
+                            <label class="main">KVK nummer</label>
+                            <input name="kvknr" type="text" placeholder="Uw KVK-nummer">
+                            <span class="error"></span>
+                        </div>
 
-                    <div class="field">
-                        <label class="main">Postcode</label>
-                        <input name="zip" type="text" placeholder="0000AA">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">Plaats</label>
-                        <input name="name" type="text" placeholder="Uw woonplaats">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field buttons">
-                        <label class="main">&nbsp;</label>
-                        <button type="button" class="prev">&laquo; Vorige</button>
-                        <button type="button" class="next">Volgende &raquo;</button>
-                    </div>
-
-
-                </section>
-
-                <!-- step 3 -->
-
-                <section class="idealsteps-step">
-
-                    <div class="field">
-                        <label class="main">Telefoon</label>
-                        <input name="phone" type="text" placeholder="000-000-0000">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">Mobiel</label>
-                        <input name="phone" type="text" placeholder="000-000-0000">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">Fax</label>
-                        <input name="phone" type="text" placeholder="000-000-0000">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field">
-                        <label class="main">E-Mail</label>
-                        <input name="email" type="email">
-                        <span class="error"></span>
-                    </div>
-
-                    <div class="field buttons">
-                        <label class="main">&nbsp;</label>
-                        <button type="button" class="prev">&laquo; Vorige</button>
-                        <button type="button" class="next">Volgende &raquo;</button>
-                    </div>
+                        <div class="field buttons">
+                            <label class="main">&nbsp;</label>
+                            <button type="button" class="next">Volgende &raquo;</button>
+                        </div>
 
 
-                </section>
+                    </section>
 
-                <!-- Step 4 -->
+                    <!-- Step 2 -->
 
-                <section class="idealsteps-step">
+                    <section class="idealsteps-step">
 
-                    <div class="field">
-                        <label class="main">Toelichting</label>
-                        <textarea name="comments" cols="30" rows="10" placeholder="Graag invullen waarom u een subsidie nodig heeft. Geef een beschijving van de beoogde activiteiten en een beschrijving van de beoogde resultaten."></textarea>
-                        <span class="error"></span>
-                    </div>
+                        <div class="field">
+                            <label class="main">Straatnaam</label>
+                            <input name="straatnaam" type="text" placeholder="Uw straatnaam met huisnummer">
+                            <span class="error"></span>
+                        </div>
 
-                    <div class="field buttons">
-                        <label class="main">&nbsp;</label>
-                        <button type="button" class="prev">&laquo; Vorige</button>
-                        <button type="submit" class="submit">Versturen</button>
-                    </div>
+                        <div class="field">
+                            <label class="main">Postcode</label>
+                            <input name="zip" type="text" placeholder="0000AA">
+                            <span class="error"></span>
+                        </div>
 
-                </section>
+                        <div class="field">
+                            <label class="main">Plaats</label>
+                            <input name="name" type="text" placeholder="Uw woonplaats">
+                            <span class="error"></span>
+                        </div>
 
-            </div>
+                        <div class="field buttons">
+                            <label class="main">&nbsp;</label>
+                            <button type="button" class="prev">&laquo; Vorige</button>
+                            <button type="button" class="next">Volgende &raquo;</button>
+                        </div>
 
-            <span id="invalid"></span>
 
-        </form>
+                    </section>
+
+                    <!-- step 3 -->
+
+                    <section class="idealsteps-step">
+
+                        <div class="field">
+                            <label class="main">Telefoon</label>
+                            <input name="phone" type="text" placeholder="000-000-0000">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="field">
+                            <label class="main">Mobiel</label>
+                            <input name="phone" type="text" placeholder="000-000-0000">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="field">
+                            <label class="main">Fax</label>
+                            <input name="phone" type="text" placeholder="000-000-0000">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="field">
+                            <label class="main">E-Mail</label>
+                            <input name="email" type="email">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="field buttons">
+                            <label class="main">&nbsp;</label>
+                            <button type="button" class="prev">&laquo; Vorige</button>
+                            <button type="button" class="next">Volgende &raquo;</button>
+                        </div>
+
+
+                    </section>
+
+                    <!-- Step 4 -->
+
+                    <section class="idealsteps-step">
+
+                        <div class="field">
+                            <label class="main">Toelichting</label>
+                            <textarea name="comments" cols="30" rows="10" placeholder="Graag invullen waarom u een subsidie nodig heeft. Geef een beschijving van de beoogde activiteiten en een beschrijving van de beoogde resultaten."></textarea>
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="field buttons">
+                            <label class="main">&nbsp;</label>
+                            <button type="button" class="prev">&laquo; Vorige</button>
+                            <button type="submit" class="submit">Versturen</button>
+                        </div>
+
+                    </section>
+
+                </div>
+
+                <span id="invalid"></span>
+
+            </form>
+
+        </div>
 
     </div>
+</div>
+
 
 </div>
 
