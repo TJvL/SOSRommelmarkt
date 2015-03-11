545e7ad3ad537772ae0fa8427c7dfbdf57e1b252
Ivo Hendriks <ihendrik1@avans.nl>
2015-03-11 23:33:29 +0100

    eerste opzet subsidie beheer

---
+++ subvention_overview.php
@@ -0,0 +1,118 @@
+<?php
+include("includes/config.inc.php");
+include("includes/markup/header.php");
+?> 
+
+<head>
+	<script src="<?php echo ROOT_DIR; ?>/includes/js/subvention_overview.js" type="text/javascript"></script>
+</head>
+
+<div class="container">
+	<div class="white">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="list-group">
+					<a class="list-group-item">
+						<h4 class="list-group-item-heading">Het Simon</h4>
+						<p class="list-group-item-text">Gewoon een baas dit bedrijf</p>
+						<div class="collapse">
+							<table class="table table-condensed">
+								<tbody>
+									<tr>
+										<th scope="row">Naam</th>
+										<td>Simon</td>
+									</tr>
+									<tr>
+										<th scope="row">Onderneming</th>
+										<td>Het Simon</td>
+									</tr>
+									<tr>
+										<th scope="row">KVK</th>
+										<td>82389</td>
+									</tr>
+									<tr>
+										<th scope="row">Adres</th>
+										<td>Baasstraat</td>
+									</tr>
+									<tr>
+										<th scope="row">Postcode</th>
+										<td>2727HB</td>
+									</tr>
+									<tr>
+										<th scope="row">Plaats</th>
+										<td>Baasstad</td>
+									</tr>
+									<tr>
+										<th scope="row">Telefoonnummer</th>
+										<td>8928932389</td>
+									</tr>
+									<tr>
+										<th scope="row">Toelichting</th>
+										<td>Gewoon een baas dit bedrijf</td>
+									</tr>
+								</tbody>
+							</table>
+							<div class="row">
+								<div class="col-sm-8"><!-- just a spacer --></div>
+								<div class="col-sm-2"><button type="button" class="btn btn-default btn-block btn-success"><span class="glyphicon glyphicon-ok-circle"></span> Accepteer</button></div>
+								<div class="col-sm-2"><button type="button" class="btn btn-default btn-block btn-danger"><span class="glyphicon glyphicon-ban-circle"></span> Weiger</button></div>
+							</div>
+						</div>
+					</a>
+					
+					<a class="list-group-item collapse-group">
+						<h4 class="list-group-item-heading">J. Janssen</h4>
+						<p class="list-group-item-text">Subsidieaanvraag preview tekst ofzo.</p>
+						<div class="collapse">
+							<table class="table table-condensed">
+								<tbody>
+									<tr>
+										<th scope="row">Naam</th>
+										<td>Simon</td>
+									</tr>
+									<tr>
+										<th scope="row">Onderneming</th>
+										<td>Het Simon</td>
+									</tr>
+									<tr>
+										<th scope="row">KVK</th>
+										<td>82389</td>
+									</tr>
+									<tr>
+										<th scope="row">Adres</th>
+										<td>Baasstraat</td>
+									</tr>
+									<tr>
+										<th scope="row">Postcode</th>
+										<td>2727HB</td>
+									</tr>
+									<tr>
+										<th scope="row">Plaats</th>
+										<td>Baasstad</td>
+									</tr>
+									<tr>
+										<th scope="row">Telefoonnummer</th>
+										<td>8928932389</td>
+									</tr>
+									<tr>
+										<th scope="row">Toelichting</th>
+										<td>Gewoon een baas dit bedrijf</td>
+									</tr>
+								</tbody>
+							</table>
+							<div class="row">
+								<div class="col-sm-8"><!-- just a spacer --></div>
+								<div class="col-sm-2"><button type="button" class="btn btn-default btn-block btn-success"><span class="glyphicon glyphicon-ok-circle"></span> Accepteer</button></div>
+								<div class="col-sm-2"><button type="button" class="btn btn-default btn-block btn-danger"><span class="glyphicon glyphicon-ban-circle"></span> Weiger</button></div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+include("includes/markup/footer.php"); 
+?> 
