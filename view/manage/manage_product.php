22f29b6ae6eec879ad634ddacfa2902dc230987d
TJvL <tjleeuwe1@student.avans.nl>
2015-03-13 10:05:32 +0100

    manage products mvc switch

---
+++ view/manage/manage_product.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="white">
+        <div class="table-responsive padding-sm">
+            <table id="productTable" class="display">
+                <thead>
+                <tr>
+                    <th>product id</th>
+                    <th>naam</th>
+                    <th>kleur code</th>
+                    <th>toegevoegd door</th>
+                    <th>optie</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                    $products = Product::fetchAllProducts();
+                    foreach ($products as $product){
+                        echo "<tr>";
+                        echo "<td>" . $product->id . "</td>";
+                        echo "<td>" . $product->name . "</td>";
+                        echo "<td>" . $product->colorCode . "</td>";
+                        echo "<td>" . $product->addedBy . "</td>";
+                        echo "<td><a href='#'>details</a></td>";
+                        echo "</tr>";
+                    }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready( function () {
+        $('#productTable').DataTable();
+    } );
+</script>
