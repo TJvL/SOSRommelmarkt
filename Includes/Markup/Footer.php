474ebd9783aca665ca7295e8fef6bbf1e2aaa340
Mirwais <mirwais_fazli@live.nl>
2015-03-23 12:36:44 +0100

    Partners - done

--- Includes/Markup/Footer.php
+++ Includes/Markup/Footer.php
@@ -2,32 +2,48 @@
 
 <div id="footer-wrapper">
     <div id="footer" class="container">
-        <div class="col-sm-3 col-md-6">
-                            
+        <div class="col-sm-3 col-md-4">
             <div class="title">
 				<h2>Contact</h2>
 			</div>
-			<ul class="style1">
-				<li>
+        </div>
+        <div class="col-sm-3 col-md-8">
+            <div class="title">
+                <h2>Partners</h2>
+            </div>
+        </div>
+        <div class="col-sm-3 col-md-4">
+            <ul class="style1">
+                <li>
                     Ridderspoorstraat 2</br>
                     5212 XP 's-Hertogenbosch </br>
                     tel: 073 613 3774 </br>
                     email: info@sosrommelmarkt.nl
                 </li>
-			</ul>
-        </div>	
-        <div class="col-sm-3 col-md-6">
-            <div class="title">
-                <h2>Overig</h2>
-            </div>
-                <ul class="style1">
-                    <li><a href="#">Semper mod quis eget mi dolore</a></li>
-                    <li><a href="#">Quam turpis feugiat sit dolor</a></li>
-                    <li><a href="#">Amet ornare in hendrerit in lectus</a></li>
-                </ul>
+            </ul>
+        </div>
+        <div class="col-sm-3 col-md-4">
+            <ul class="style1">
+                <li><a href="http://www.cvtb.nl">CVTB (Centrum voor Trajecten en Bemoeizorg)</a></li>
+                <li><a href="http://www.sosrommelmarkt.nl/organisatie/samenwerkingen/www.cwinet.nl=">CWI (Centrum voor Werk en Inkomen)</a></li>
+                <li><a href="http://www.modenbosch.nl/">SMO (Stichting Maatschappelijke Opvang)</a></li>
+                <li><a href="http://www.weenerxl.nl/">Weenergoep</a></li>
+                <li><a href="http://www.lokaalloket.nl/s-hertogenbosch">WMO loket</a></li>
+
+            </ul>
+        </div>
+        <div class="col-sm-3 col-md-4">
+            <ul class="style1">
+                <li><a href="http://http://www.juvans.nl/index.php?p=183">Juvans (maatschappelijk werk ‘s-Hertogenbosch)</a></li>
+                <li><a href="http://www.giralisgroep.nl/">GS (Geestelijke Gezondheidszorg)</a></li>
+                <li><a href="http://www.novadic-kentron.nl/">Novadic-Kentron Verslavingszorg</a></li>
+                <li><a href="http://www.uwv.nl/particulieren/">UWV</a></li>
+                <li><a href="http://www.ambitie.org/">Ambitie</a></li>
+                <li><a href="http://www.s-hertogenbosch.nl/">Sociale dienst</a></li>
+            </ul>
         </div>
     </div>
-</div>    
+</div>
 
 <div id="copyright">
 	<p>&copy; SOSrommelmarkt. All rights reserved. | Design by 42IN07SOe</a>.</p>
