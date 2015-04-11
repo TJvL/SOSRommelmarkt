dac53d5fa7ada305a609128f857234ca829cb81a
Ivo Hendriks <ihendrik1@avans.nl>
2015-04-11 18:15:44 +0200

    Carousel Compleet (opmaak)
    nb. moet nog data uit DB ophalen

--- view/home/index.php
+++ view/home/index.php
@@ -44,31 +44,29 @@
         </div>
     </div>
 
-    <div class="grey">
+    <div class="grey padding-sm">
     	<div class="row">
-    		<div class="col-md-12">
+    		<div class="col-md-6">
 	    		<div id="vitrine-carousel" class="carousel slide" data-ride="carousel">
 			    	<ol class="carousel-indicators">
 			    		<li data-target="#vitrine-carousel" data-slide-to="1"></li>
 			    		<li data-target="#vitrine-carousel" data-slide-to="2"></li>
+			    		<li data-target="#vitrini-carousel" data-slide-to="3"></li>
 			    	</ol>
 			    
 				    <div class="carousel-inner" role="listbox">
 				    	<div class="item active">
-				    		<img src="<?php echo ROOT_DIR;?>/img/tempslideshow" alt="Slide 1">
-				    		<div class="carousel-caption">
-				    			<h3>$_productNaam</h3>
-				    			<p>$_productText</p>
-				    		</div>
+				    		<img src="<?php echo ROOT_DIR;?>/img/tempslideshow/slideshow_1140x456_1.jpg" alt="Slide 1">
 				    	</div>
+				    	
 				    	<div class="item">
-				    		<img src="<?php echo ROOT_DIR;?>/imge/slide2.png" alt="Slide 2">
-				    		<div class="carousel-caption">
-				    			<h3>$_productNaam</h3>
-				    			<p>$_productText</p>
-				    		</div>
+				    		<img src="<?php echo ROOT_DIR;?>/img/tempslideshow/slideshow_1140x456_2.jpg" alt="Slide 2">
 				    	</div>
-				    </div>
+				    	
+				    	<div class="item">
+				    		<img src="<?php echo ROOT_DIR;?>/img/tempslideshow/slideshow_1140x456_3.jpg" alt="Slide 3">
+				    	</div>
+			    	</div>
 				    
 				    <a class="left carousel-control" href="#vitrine-carousel" role="button" data-slide="prev">
 				    	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -79,6 +77,11 @@
 				    	<span class="sr-only">Volgende</span>
 				    </a>
 			    </div>
+    		</div>
+    		<div class="col-md-6">
+    			<h1>Veiling</h1>
+    			<h2>$_productNaam</h2>
+    			<p>$_productBeschrijving</p>
     		</div>
     	</div>
     </div>
