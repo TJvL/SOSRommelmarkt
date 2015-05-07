9bb11f1de84e915076bf9c3c60fb6d3b1eaa3be3
Ivo Hendriks <ihendrik1@student.avans.nl>
2015-05-07 19:18:46 +0200

    layout van addpartner pagina gewijzigd

--- application/view/manage/addpartner.php
+++ application/view/manage/addpartner.php
@@ -7,33 +7,25 @@
         </div>
         <div class="row">
             <hr>
-            <div class="col-sm-1"></div>
-            <h1>Partner toevoegen</h1>
+            <div class="col-sm-offset-2 col-sm-10">
+	            <h1>Partner toevoegen</h1>
+            </div>
             <form class="form-horizontal" id="partnerForm" method="POST" action="<?php echo ROOT_PATH . "/manage/addpartner" ?>">
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Naam</label>
-                    <div class="col-sm-10">
-                        <div class="input-group">
-                            <input class="form-control" form="partnerForm" name="name" type="text" placeholder="Naam van de partner" required>
-                        </div>
+                    <label class="control-label col-sm-2" for="name">Naam</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" form="partnerForm" id="name" name="name" type="text" placeholder="Naam van de partner" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2">Website</label>
-                    <div class="col-sm-10">
-                        <div class="input-group">
-                            <textarea class="form-control" form="partnerForm" name="website" style="resize: none" rows="3" placeholder="Website van de partner" required></textarea>
-                        </div>
+                    <label class="control-label col-sm-2" for="website">Website</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" form="partnerForm" id="website" name="website" type="text" placeholder="Website van de partner" required></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2"></label>
-                    <div class="col-sm-10">
-                        <div class="input-group">
-                            <div class="btn-toolbar">
-                                <button class="btn btn-default" form="partnerForm" type="submit">Opslaan</button>
-                            </div>
-                        </div>
+                    <div class="col-sm-offset-2 col-sm-2">
+                        <button class="btn btn-default btn-block" form="partnerForm" type="submit">Opslaan</button>
                     </div>
                 </div>
             </form>
