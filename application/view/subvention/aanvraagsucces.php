4360eb56738b8596e8971067c51ddb9948e6452a
BramBrouwer <b.brouwer@student.avans.nl>
2015-05-14 14:28:24 +0200

    Landingspagina subsidieaanvraag gefixt

--- application/view/subvention/aanvraagsucces.php
+++ application/view/subvention/aanvraagsucces.php
@@ -1,10 +1,13 @@
-<!--echo $viewbag['voorbeeld'];-->
-<div class="padding-hor-md">
-			<div class="white margin-ver-lg">
-				<h2>Subsidie</h2>
-				<p>Uw aanvraag is succesvol opgeslagen en wordt spoedig in behandeling genomen.</p>
 
-            </div>
-		</div>
+<div class="container">
+<div class="white">
+	<div class="row">
 
-
+			<div class="col-md-12">
+				<h4>Uw aanvraag is succesvol opgeslagen en wordt spoedig in behandeling genomen.</h4>
+			</div>
+		<div class="col-ms-9"><!-- spacer --></div>
+		<div class="col-md-3"><a href="/SOSRommelmarkt/home"><button type="button" class="btn btn-red btn-lg">Home<i class="fa fa-chevron-right"></i></button></a></div>
+	</div>
+</div>
+</div>
