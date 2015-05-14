e79cd3f56c9b466e31d71ff77c3675f9f26acd23
BramBrouwer <b.brouwer@student.avans.nl>
2015-05-14 14:40:00 +0200

    Onnodige shit uti header gehaald

--- Includes/header.inc.php
+++ Includes/header.inc.php
@@ -7,36 +7,15 @@
     <title>SOS Rommelmarkt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!--
-    Wat is dit?
-    -Thomas
-    -->
     <link href = "<?php echo ROOT_PATH; ?>application/model/database/database.class.php" rel = "database">
-
-
     <?php
     IncludeLocator::locateIncludes("css", $controller, $action);
     ?>
-
     <?php
     IncludeLocator::locateIncludes("js", $controller, $action);
     ?>
     <script src="<?php echo ROOT_PATH; ?>/frameworks/idealforms/js/out/jquery.idealforms.js" type="text/javascript"></script>
 
-<!--    <link rel="stylesheet" href="--><?php //echo ROOT_DIR; ?><!--css/menu.css" type="text/css" media="screen">-->
-<!--    <link rel="stylesheet" href="--><?php //echo ROOT_DIR; ?><!--fonts/font-awesome/css/font-awesome.min.css" >-->
-<!--    <link href = "--><?php //echo ROOT_DIR; ?><!--frameworks/bootstrap/css/bootstrap.min.css" rel = "stylesheet">-->
-<!--    <link href = "--><?php //echo ROOT_DIR; ?><!--css/shoppingcart.css" rel = "stylesheet">-->
-<!--    <link href = "--><?php //echo ROOT_DIR; ?><!--css/footer.css" rel = "stylesheet">-->
-<!--    <link href=  "--><?php //echo ROOT_DIR; ?><!--css/jquery.bxslider.css" rel="stylesheet">-->
-<!--    <link href=  "--><?php //echo ROOT_DIR; ?><!--css/style.css" rel="stylesheet">-->
-<!--    <link href=  "--><?php //echo ROOT_DIR; ?><!--css/vitrine.css" rel="stylesheet">-->
-<!--    <link href=  "--><?php //echo ROOT_DIR; ?><!--css/angular.rangeSlider.css" rel="stylesheet">-->
-<!--    <link href = "--><?php //echo ROOT_DIR; ?><!--css/carousel.css" rel="stylesheet">-->
-<!--    <link href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" rel="stylesheet">-->
-<!--    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css">-->
-<!--    <link href="--><?php //echo ROOT_DIR; ?><!--frameworks/IdealForms/css/jquery.idealforms.css" rel="stylesheet">-->
-<!--    <link href="--><?php //echo ROOT_DIR; ?><!--css/override-bootstrap.css" rel="stylesheet">-->
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -45,39 +24,16 @@
     <![endif]-->
 
 
-<!--    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
-<!---->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/shopfilter.js" type="text/javascript"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/shoppingcart.js" type="text/javascript"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--frameworks/bootstrap/js/bootstrap.min.js"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/jquery.bxslider.min.js"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/menu.js" type="text/javascript"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/vitrine.js" type="text/javascript"></script>-->
-<!--    <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js" type="text/javascript"></script>-->
-<!---->
-<!--    <script src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.js" type="text/javascript"></script>-->
-<!--    <script src="//cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>-->
-<!--    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.js"></script>-->
-<!--    <script src="--><?php //echo ROOT_DIR; ?><!--js/shop/angular.rangeSlider.js" type="text/javascript"></script>-->
-<!--    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular-animate.js"></script>-->
-
 </head>
 <body>
     <nav class= "navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
-<!--                <button class = "navbar-toggle" data-toggle = "collapse" data-target = "#navbar">-->
-<!--                    <span class="icon-bar"></span>-->
-<!--                    <span class="icon-bar"></span>-->
-<!--                    <span class="icon-bar"></span>-->
-<!--                </button>-->
             </div>
-            
             <?php 
             	// contactinfo ophalen
             	$companyInformation = CompanyInformation::selectCurrent();
             ?>
-
             <div id="navbar" class=" navbar">
                 <ul class = "nav navbar-nav ">
                     <li>
@@ -91,13 +47,6 @@
                         </div>
                     </li>
                 </ul>
-<!--                <ul class="nav navbar-nav navbar-left">-->
-<!--                    <li><a href="--><?php //echo ROOT_DIR; ?><!--/home/index"><i class="fa fa-home"></i> Home</a></li>-->
-<!--                    <li><a  href="--><?php //echo ROOT_DIR; ?><!--/shop/index"><i class="fa fa-cubes"></i> Producten</a></li>-->
-<!--                    <li><a href="--><?php //echo ROOT_DIR; ?><!--/subvention/index"><i class="fa fa-money"></i> Subsidie</a></li>-->
-<!--                    <li><a href="--><?php //echo ROOT_DIR; ?><!--/aboutUs/index"><i class="fa fa-group"></i> Over ons</a></li>-->
-<!--                    <li><a  href="--><?php //echo ROOT_DIR; ?><!--/contact/index"><i class="fa fa-envelope"></i> Contact</a></li>-->
-<!--                </ul>-->
             </div>
         </div>
     </nav>
