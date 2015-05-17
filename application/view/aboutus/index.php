458a46737d992eb68430e5c02cd624354f87df65
Ivo Hendriks <ihendrik1@student.avans.nl>
2015-05-17 21:34:02 +0200

    dingenenzo

--- application/view/aboutus/index.php
+++ application/view/aboutus/index.php
@@ -1,22 +1,18 @@
 <div class="container">
+	<?php
+	// Get all modules that belong to about us
+	$modules = Module::SelectByCategory("aboutus");
 
+	for ($i = 0; $i < count($modules); $i++) {
 
-        <?php
-        // Get all modules that belong to about us
-        $modules = Module::SelectByCategory("aboutus");
-
-        for ($i = 0; $i < count($modules); $i++) {
-
-        ?>
-            <div style="margin-bottom:5%" class="row white margin-hor-0">
-                <div class="row">
-
-                    <div class="col-md-12">
-                        <div class="white margin-ver-lg ">
-                            <h2><?php echo $modules[$i]->heading; ?></h2>
-                            <p class="equal-height"><?php echo $modules[$i]->content; ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
+	?>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="white">
+				<h2><?php echo $modules[$i]->heading; ?></h2>
+				<p class="equal-height"><?php echo $modules[$i]->content; ?></p>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+</div>
