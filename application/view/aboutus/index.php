b7077679a05cfe580a1180c47efc522a36876f89
Mirwais <mirwais_fazli@live.nl>
2015-05-17 21:26:32 +0200

    fjfgfgjh

--- application/view/aboutus/index.php
+++ application/view/aboutus/index.php
@@ -1,45 +1,22 @@
 <div class="container">
-	<div style="margin-bottom:5%" class="row white margin-hor-0">
 
 
+        <?php
+        // Get all modules that belong to about us
+        $modules = Module::SelectByCategory("aboutus");
 
-			<style>
-.img-anbi {
-    float: right;
-    height: 200px !important;
-    width: auto !important;
-    border: 1px dotted black;
-    margin: 0px 0px 15px 20px;
-}
-</style>
-			<ol type="1">
-			  <li>het aandacht geven aan en het verzorgen van belangenbehartiging van mensen die door omstandigheden aan de “onderkant” van de samenleving belanden/zijn beland;</li>
-			  <li>het bieden van dagbesteding/inloopfunctie en het reactiveren en begeleiden van mensen met problemen op een of meerdere leefgebieden;</li>
-			  <li>het genereren van gelden voor het doen van bijdragen aan projecten in ontwikkelingslanden.</li>
-			</ol>
-			
-			<p>SOS Rommelmarkt is onderdeel van de Bondgenoten Sociale Activering (BOSA). In ’s-Hertogenbosch leiden honderden inwoners een bestaan aan de rand van de samenleving. Ze hebben doorgaans geen werk of zelfs geen vaste verblijfplaats. Ze hebben een gevangenisstraf achter de rug en kunnen maar met moeite de draad weer oppakken. Ze kampen met gezondheidsproblemen, vaak als gevolg van drugsgebruik. Ze hebben een chronisch gebrek aan inkomen, omdat ze niet voldoen aan de voorwaarden voor een bijstandsuitkering. Meestal gaat het om een combinatie van problemen.</p>
-			</br>
-			<p>Sinds 2004 werken in ’s-Hertogenbosch een aantal organisaties samen in BOSA, Bondgenoten Sociale Activering. De BOSA-organisaties beginnen waar de gangbare hulpverleningsinstellingen het contact hebben verloren. Zonder veel voorwaarden, wachtlijsten of bureaucratie. Door gewoon te kijken wat iemand kan en wil, op eigen kracht; met een steuntje in de rug, maar zonder dwang. Door open en eerlijk te zijn, en niet te overvragen</p>
-			</br>
-			<p>De BOSA-organisaties zijn op de eerste plaats een veilige haven, waar mensen hun verhaal kunnen doen, even op adem komen, samen met anderen iets doen en ook nog wat bijverdienen. Voor velen is dat het begin van een nieuw leven. Dankzij het respect van medewerkers en talrijke vrijwilligers ontwikkelt zich (weer) zelfvertrouwen. Arbeid en dagbesteding brengen structuur in het dagelijks leven. Soms vindt iemand zelfs een baan; de meesten zetten kleinere stappen.</p>
+        for ($i = 0; $i < count($modules); $i++) {
 
-			
-				<p class="paragraph">In BOSA werken de volgende organisaties samen:
-				  <ul>
-				    <li>Stichting Pastoraal Uitzendbureau (PUB)</li>
-				    <li>Stichting SOS Rommelmarkt</li>
-				    <li>Vincentiusvereniging</li>
-				    <li>Stichting De Zelfkrant</li>
-				    <li>Jong actief</li>
-				    <li>de Voedselbank</li>
-				    <li>Van harte Resto</li>
-				    <li>de Werkcarroussel</li>
-				  </ul>
-				</p>
+        ?>
+            <div style="margin-bottom:5%" class="row white margin-hor-0">
+                <div class="row">
 
-			<p>Door onderlinge samenwerking is het bijvoorbeeld mogelijk om tijdens de koude wintermaanden te zorgen voor opvang tijdens de uren dat andere instellingen gesloten zijn.</p>
-
-		</div>
-	</div>
-</div>		
+                    <div class="col-md-12">
+                        <div class="white margin-ver-lg ">
+                            <h2><?php echo $modules[$i]->heading; ?></h2>
+                            <p class="equal-height"><?php echo $modules[$i]->content; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
