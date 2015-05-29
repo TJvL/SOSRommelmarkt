cc878fa11ce2bc0f873d6af785a414478b24e076
Erik--Vink <jhvink@student.avans.nl>
2015-05-29 10:06:46 +0200

    Menu volgorde (feedback klant @ blackboard)
    Menu volgorde (feedback klant @ blackboard)

--- Includes/header.inc.php
+++ Includes/header.inc.php
@@ -74,10 +74,10 @@ IncludeLocator::locateIncludes("js", $controller, $action);
                                          <li><a href="<?php echo ROOT_PATH; ?>/home/index"><i class="fa fa-home"></i> Home</a></li>
                                          <li><a href="<?php echo ROOT_PATH; ?>/shop/index"><i class="fa fa-cubes"></i> Webshop</a></li>
                                          <li><a href="<?php echo ROOT_PATH; ?>/auction/index"><i class="fa fa-eye"></i> Vitrine</a></li>
-                                         <li><a href="<?php echo ROOT_PATH; ?>/subvention/index"><i class="fa fa-money"></i> Subsidie</a></li>
-                                         <li><a href="<?php echo ROOT_PATH; ?>/home/aboutus"><i class="fa fa-group"></i> Over ons</a></li>
-                                         <li><a href="<?php echo ROOT_PATH; ?>/home/projects"><i class="fa fa-tasks"></i> Projecten</a></li>
                                          <li><a href="<?php echo ROOT_PATH; ?>/home/retrieval"><i class="fa fa-truck"></i> Afgifte</a></li>
+                                         <li><a href="<?php echo ROOT_PATH; ?>/subvention/index"><i class="fa fa-money"></i> Subsidie</a></li>
+                                         <li><a href="<?php echo ROOT_PATH; ?>/home/projects"><i class="fa fa-tasks"></i> Projecten</a></li>
+                                         <li><a href="<?php echo ROOT_PATH; ?>/home/aboutus"><i class="fa fa-group"></i> Over ons</a></li>
                                      </ul>
                                  <?php
                                  }
