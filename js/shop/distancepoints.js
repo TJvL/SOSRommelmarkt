978456cdb0bf0976bde17f0eda29d0f9014f07f1
Issy <iekici@avans.nl>
2015-05-31 15:38:59 +0200

    distance between two points

---
+++ js/shop/distancepoints.js
@@ -0,0 +1,8 @@
+map.({
+  // // origin: [-12.044012922866312, -77.02470665341184],
+  // destination: [-12.090814532191756, -77.02271108990476],
+  travelMode: 'driving',
+  strokeColor: '#131540',
+  strokeOpacity: 0.6,
+  strokeWeight: 6
+});
