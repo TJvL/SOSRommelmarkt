d222eb59fb774b808085f468f4ae6bb55d243d40
Erik--Vink <jhvink@student.avans.nl>
2015-06-01 09:22:44 +0200

    Parnter add fix2
    Parnter add fix2

--- application/view/manage/addpartner.php
+++ application/view/manage/addpartner.php
@@ -26,6 +26,7 @@
                 <div class="form-group">
                     <label class="control-label col-sm-2" for="categorie">Categorie</label>
                     <div class="col-sm-8">
+                        <select class="form-control" form="createPartnerForm" name="category" id="category">
                             <option value="disabled" disabled selected>Kies een partner categorie</option>
                             <option value="sos">SOS</option>
                             <option value="dienstverleners">Dienstverleners</option>
