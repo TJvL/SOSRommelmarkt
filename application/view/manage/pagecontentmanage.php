d4b42d7e6d1920613edee22b2364eafab734f689
Ivo Hendriks <ihendrik1@student.avans.nl>
2015-06-23 18:14:43 +0200

    Fixed tabs in manage-pages

--- application/view/manage/pagecontentmanage.php
+++ application/view/manage/pagecontentmanage.php
@@ -14,78 +14,83 @@
             </ul>
             <!-- end nav tabs -->
             <!-- start tab panes -->
-            <div role="tabpanel" class="tab-pane fade margin-ver-lg" id="home-modules">
-                <?php $modules = $model->homeModules; ?>
-                <div class="row">
-                    <div class="col-md-1">
-                        <a href="<?php echo ROOT_PATH . '/manage/moduleadd/home'?>" class="btn btn-success">Nieuwe module</a>
-                    </div>
-                </div>
-                <div class="table-responsive padding-sm margin-lg">
-                    <table id="homeTable" class="display">
-                        <thead>
-                        <tr>
-                            <th>Titel</th>
-                            <th>Opties</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach($model->homeModules as $module){ ?>
-                            <tr>
-                                <td><?php echo $module->heading ?></td>
-                                <td>
-                                    <a href="<?php echo ROOT_PATH . '/manage/moduleview/' . $module->id?>"> <button class="btn btn-default" title="Aanpassen"><i class="fa fa-pencil"></i></button></a>
-                                    <!-- Tijdelijk verwijderd -- werken nog niet -->
-                                    <!--										<button class="btn btn-default --><?php //if ($i == 0) echo "disabled"; ?><!--" title="Omhoog" onClick="moveModuleUp()"><i class="fa fa-caret-up"></i></button>-->
-                                    <!--										<button class="btn btn-default --><?php //if ($i == count($modules) - 1) echo "disabled"; ?><!--" title="Omlaag" onClick="moveModuleDown()"><i class="fa fa-caret-down"></i></button>-->
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 padding-lg">
-                        <p id="statushomemodules" class="padding-lg"></p>
-                    </div>
-                </div>
-            </div>
-
-            <div role="tabpanel" class="tab-pane fade margin-ver-lg" id="aboutus-modules">
-                <?php $modules = $model->aboutUsModules ?>
-                <div class="row">
-                    <div class="col-md-1">
-                        <a href="<?php echo ROOT_PATH . '/manage/moduleadd/aboutus' ?>" class="btn btn-success">Nieuwe module</a>
-                    </div>
-                </div>
-                <div class="table-responsive padding-sm margin-lg">
-                    <table id="aboutusTable" class="display">
-                        <thead>
-                        <tr>
-                            <th>Heading</th>
-                            <th>Opties</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach($model->aboutUsModules as $module){ ?>
-                            <tr>
-                                <td><?php echo $module->heading ?></td>
-                                <td>
-                                    <a href="<?php echo ROOT_PATH . '/manage/moduleview/' . $module->id?>"> <button class="btn btn-default" title="Aanpassen"><i class="fa fa-pencil"></i></button></a>
-                                    <!-- Tijdelijk verwijderd -- werken nog niet-->
-                                    <!--										<button class="btn btn-default --><?php //if ($i == 0) echo "disabled"; ?><!--" title="Omhoog" onClick="moveModuleUp()"><i class="fa fa-caret-up"></i></button>-->
-                                    <!--										<button class="btn btn-default --><?php //if ($i == count($modules) - 1) echo "disabled"; ?><!--" title="Omlaag" onClick="moveModuleDown()"><i class="fa fa-caret-down"></i></button>-->
-
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 padding-lg">
-                        <p id="statusaboutusmodules" class="padding-lg"></p>
-                    </div>
-                </div>
+            <div class="tab-content">
+	            <div role="tabpanel" class="tab-pane fade in active margin-ver-lg" id="home-modules">
+	                <?php $modules = $model->homeModules; ?>
+	                <div class="row">
+	                    <div class="col-md-1">
+	                        <a href="<?php echo ROOT_PATH . '/manage/moduleadd/home'?>" class="btn btn-success">Nieuwe module</a>
+	                    </div>
+	                </div>
+	                <div class="table-responsive padding-sm margin-lg">
+	                    <table id="homeTable" class="display">
+	                        <thead>
+	                        <tr>
+	                            <th>Titel</th>
+	                            <th>Opties</th>
+	                        </tr>
+	                        </thead>
+	                        <tbody>
+	                        <?php foreach($model->homeModules as $module){ ?>
+	                            <tr>
+	                                <td><?php echo $module->heading ?></td>
+	                                <td>
+	                                    <a href="<?php echo ROOT_PATH . '/manage/moduleview/' . $module->id?>"> <button class="btn btn-default" title="Aanpassen"><i class="fa fa-pencil"></i></button></a>
+	                                    <!-- Tijdelijk verwijderd -- werken nog niet -->
+	                                    <!--										<button class="btn btn-default --><?php //if ($i == 0) echo "disabled"; ?><!--" title="Omhoog" onClick="moveModuleUp()"><i class="fa fa-caret-up"></i></button>-->
+	                                    <!--										<button class="btn btn-default --><?php //if ($i == count($modules) - 1) echo "disabled"; ?><!--" title="Omlaag" onClick="moveModuleDown()"><i class="fa fa-caret-down"></i></button>-->
+	                                </td>
+	                            </tr>
+	                        <?php } ?>
+	                        </tbody>
+	                    </table>
+	                </div>
+	                <div class="row">
+	                    <div class="col-md-12 padding-lg">
+	                        <p id="statushomemodules" class="padding-lg"></p>
+	                    </div>
+	                </div>
+	            </div>
+	
+	            <div role="tabpanel" class="tab-pane fade in margin-ver-lg" id="aboutus-modules">
+	                <?php $modules = $model->aboutUsModules ?>
+	                <div class="row">
+	                    <div class="col-md-1">
+	                        <a href="<?php echo ROOT_PATH . '/manage/moduleadd/aboutus' ?>" class="btn btn-success">Nieuwe module</a>
+	                    </div>
+	                </div>
+	                <div class="table-responsive padding-sm margin-lg">
+	                    <table id="aboutusTable" class="display">
+	                        <thead>
+	                        <tr>
+	                            <th>Heading</th>
+	                            <th>Opties</th>
+	                        </tr>
+	                        </thead>
+	                        <tbody>
+	                        <?php foreach($model->aboutUsModules as $module){ ?>
+	                            <tr>
+	                                <td><?php echo $module->heading ?></td>
+	                                <td>
+	                                    <a href="<?php echo ROOT_PATH . '/manage/moduleview/' . $module->id?>"> <button class="btn btn-default" title="Aanpassen"><i class="fa fa-pencil"></i></button></a>
+	                                    <!-- Tijdelijk verwijderd -- werken nog niet-->
+	                                    <!--										<button class="btn btn-default --><?php //if ($i == 0) echo "disabled"; ?><!--" title="Omhoog" onClick="moveModuleUp()"><i class="fa fa-caret-up"></i></button>-->
+	                                    <!--										<button class="btn btn-default --><?php //if ($i == count($modules) - 1) echo "disabled"; ?><!--" title="Omlaag" onClick="moveModuleDown()"><i class="fa fa-caret-down"></i></button>-->
+	
+	                                </td>
+	                            </tr>
+	                        <?php } ?>
+	                        </tbody>
+	                    </table>
+	                </div>
+	                <div class="row">
+	                    <div class="col-md-12 padding-lg">
+	                        <p id="statusaboutusmodules" class="padding-lg"></p>
+	                    </div>
+	                </div>
+	            </div>
             </div>
             <!-- end tab panes -->
+        </div>
+    </div>
+</div>
