20436887a385d81fc5a35713acf9f7294a932bbe
Issy <iekici@avans.nl>
2015-06-25 22:41:46 +0200

    add pages terug --> Annuleren

--- application/view/manage/partneradd.php
+++ application/view/manage/partneradd.php
@@ -2,7 +2,7 @@
     <div class="white">
         <div class="row">
             <div class="col-md-1">
-                <a href="<?php echo ROOT_PATH . "/manage/partneroverview" ?>" class="btn btn-default">Terug</a>
+                <a href="<?php echo ROOT_PATH . "/manage/partneroverview" ?>" class="btn btn-default">Annuleren</a>
             </div>
         </div>
         <div class="row">
