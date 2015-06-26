71b6b472d7e3278f33b2939e3cdf38e56bab4d79
Ivo Hendriks <ihendrik1@student.avans.nl>
2015-06-26 02:12:12 +0200

    merge conflict resolved

--- application/view/order/index.php
+++ application/view/order/index.php
@@ -1,40 +1,57 @@
-<?php $useCart = isset($cart); ?>
+<?php $cartExists = isset($_SESSION["cart"]);  ?>
+
 
 <div class="container">
-	<div class="white">
-		<div class="row">
-			<div class="col-lg-12">
-				<h1>Uw Winkelwagen</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-12">
-				<?php if ($useCart) { ?>
-				<table class="table table-responsive">
-					<tr>
-						<th>Artikel</th>
-						<th>Prijs</th>
-						<th><!-- buttons --></th>
-					</tr>
-					<?php foreach ($cart->cartContent as $p) { ?>
-					<tr>
-						<td>$ARTIKELNAAM</td>
-						<td>$ARTIKELPRIJS</td>
-						<td><a href="#"><button class="btn btn-default" title="Aanpassen"><i class="fa fa-pencil"></i></button></a></td><!-- add delete item onclick or whatever -->
-					<?php } ?>
-				</table>
-				<?php } else { ?>
-				<p>U heeft geen artikelen in uw winkelwagen.</p>
-				<a href="<?php echo ROOT_PATH . '/home/shop'; ?>">Bezoek de winkel</a>
-				<?php } ?>
-			</div>
-		</div>
-		<?php if ($useCart) { ?>
-		<div class="row">
-			<div class="col-lg-offset-9 col-lg-3">
-				<a class="btn btn-red btn-lg btn-block" href="<?php echo ROOT_PATH . '/order/address'; ?>">Bestellen <i class="fa fa-chevron-right"></i></a>
-			</div>
-		</div>
-		<?php } ?>
-	</div>
+    <div class="white">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Uw Winkelwagen</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <?php if ($cartExists) { ?>
+                    <table class="table table-responsive">
+                        <tr>
+                            <th>Artikel</th>
+                            <th>Prijs</th>
+                            <th><!-- buttons --></th>
+                        </tr>
+                        <?php
+                        $totalPrice = 0;
+                        foreach ($_SESSION["cart"]->cartContent as $product) { ?>
+                        <tr>
+                            <td><?php echo $product->name ?></td>
+                            <td>€<?php echo $product->price ?></td>
+                            <td><a href="#"><button class="btn btn-danger" title="Verwijder" onclick="deleteProductFromCart(<?php echo $product->id ?>)"><i class="fa fa-trash"></i></button></a></td><!-- add delete item onclick or whatever -->
+                        </tr>
+                        <?php
+                            $totalPrice+= $product->price;
+                        } ?>
+                        <tr>
+                            <td></td>
+                            <td><?php echo "Totaal: €" . number_format($totalPrice, 2, '.', ',');?></td>
+
+                            <td></td>
+                        </tr>
+                    </table>
+                <?php } else { ?>
+                    <p>U heeft geen artikelen in uw winkelwagen.</p>
+                    <a href="<?php echo ROOT_PATH . '/home/shop'; ?>">Bezoek de winkel</a>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 padding-lg">
+                <p id="status" class="padding-lg"></p>
+            </div>
+        </div>
+        <?php if ($cartExists) { ?>
+            <div class="row">
+                <div class="col-lg-offset-10 col-lg-2">
+                    <a class="btn btn-red" onclick='addNewOrder()'>Bestellen <i class="fa fa-chevron-right"></i></a>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
 </div>
